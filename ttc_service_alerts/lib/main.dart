b2111e595f97d91ad1e9252f318b64a37c489579
Stefan Urosevic <stefanuros@gmail.com>
2019-09-12 22:59:11 -0400

    SU: Added page tabs

--- ttc_service_alerts/lib/main.dart
+++ ttc_service_alerts/lib/main.dart
@@ -6,13 +6,27 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Welcome to Flutter',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Welcome to Flutter'),
-        ),
-        body: Center(
-          child: Text('Hello World'),
+      title: 'TTC Service Alerts',
+      home: DefaultTabController(
+          length: 2,
+          child: Scaffold(
+            appBar: AppBar(
+              backgroundColor: Colors.red,
+              title: Text('TTC Service Alerts'),
+              bottom: TabBar(
+                indicatorColor: Colors.white,
+                tabs: <Widget>[
+                  Tab(icon: Icon(Icons.directions_transit),),
+                  Tab(icon: Icon(Icons.notifications_active),),
+                ],
+              ),
+            ),
+            body: TabBarView(
+            children: [
+              Icon(Icons.directions_transit),
+              Icon(Icons.notifications_active),
+            ],
+          ),
         ),
       ),
     );
