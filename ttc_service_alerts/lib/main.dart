d364273623d2ef8862e3d0c26aa8c455d20e2f81
Stefan Urosevic <stefanuros@gmail.com>
2019-09-25 22:19:05 -0400

    SU: Made a page for each tab

--- ttc_service_alerts/lib/main.dart
+++ ttc_service_alerts/lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+import 'pages/FeedPage.dart';
+import 'pages/SettingsPage.dart';
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
@@ -8,23 +11,25 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'TTC Service Alerts',
       home: DefaultTabController(
-          length: 2,
-          child: Scaffold(
-            appBar: AppBar(
-              backgroundColor: Colors.red,
-              title: Text('TTC Service Alerts'),
-              bottom: TabBar(
-                indicatorColor: Colors.white,
-                tabs: <Widget>[
-                  Tab(icon: Icon(Icons.directions_transit),),
-                  Tab(icon: Icon(Icons.notifications_active),),
-                ],
-              ),
+        length: 2,
+        child: Scaffold(
+          appBar: AppBar(
+            backgroundColor: Colors.red,
+            title: Text('TTC Service Alerts'),
+            bottom: TabBar(
+              indicatorColor: Colors.white,
+              tabs: [
+                Tab(icon: Icon(Icons.directions_transit),),
+                Tab(icon: Icon(Icons.notifications_active),),
+              ],
             ),
-            body: TabBarView(
+          ),
+          body: TabBarView(
             children: [
-              Icon(Icons.directions_transit),
-              Icon(Icons.notifications_active),
+              FeedPage(),
+              SettingsPage()
+              // Icon(Icons.directions_transit),
+              // Icon(Icons.notifications_active),
             ],
           ),
         ),
---
+++ ttc_service_alerts/lib/pages/SettingsPage.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SettingsPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Icon(Icons.notifications_active),
+    );
+  }
+}
